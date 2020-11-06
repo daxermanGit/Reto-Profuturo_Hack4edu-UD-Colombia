98e1dff9bf64596c975d69eaf3174748322d2199
dacorile <31023043+dacorile@users.noreply.github.com>
2020-11-06 13:34:56 -0500

    ahora con php
    toca index

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php require_once("cloud.html") ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<html>
+  <head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HNN1JN853X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HNN1JN853X');
+</script>
+</head>
+<?php 
+require_once("cloud.html") 
+?>
+</html>
